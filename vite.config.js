a5ce2b7bbae1fd09350dc761dc90664b5994fbfa
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-04 18:57:54 +0100

    sa

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons']
-    }
-  }
+      external: ["react-icons"], // Ensure react-icons is not bundled incorrectly
+    },
+  },
 });
