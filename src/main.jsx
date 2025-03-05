3f88f2a6b3097d12fdfc965a8f49e91633270f92
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-05 23:59:30 +0100

    Fixsa: Deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
 import { Route,  Routes , BrowserRouter as Router  } from 'react-router-dom'
 import { extendedapi } from './features/appslice.jsx'
