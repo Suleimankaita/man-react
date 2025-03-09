96aa145424b2b8aac2f4c551a226c81548dacdcb
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-09 23:22:12 +0100

    sasdw

--- src/hooks/UseAuth.jsx
+++ src/hooks/UseAuth.jsx
@@ -1,51 +1,35 @@
 import { auths } from '../features/logslice'
 import { useSelector } from 'react-redux' 
-import {jwtDecode} from 'jwt-decode'
+import {jwtDecode} from 'jwt-decode' // Corrected import statement
 import { getid } from '../features/logslice' 
 import { useDispatch } from 'react-redux'
 import { io } from 'socket.io-client'
 
 const UseAuth = () => {
-  
-
-    const save=(saves)=>{
-        localStorage.setItem("roles",JSON.stringify(saves))
+    const save = (saves) => {
+        localStorage.setItem("roles", JSON.stringify(saves))
     }
-    
 
-    const token=useSelector(auths)
-    let Isadmin=false;
-    let User=false;
+    const token = useSelector(auths)
+    let Isadmin = false;
+    let User = false;
 
-    if(token){
+    if (token) {
+        const docode = jwtDecode(token)                
+        const { username, Roles, id, account, update } = docode.UserInfo;
 
-        const docode=jwtDecode(token)                
-        
-        const {username,Roles,id, account,update}=docode.UserInfo;
-        
-           
+        Isadmin = Roles.includes("Admin")
+        User = Roles.includes("User")
 
-        
-        Isadmin=Roles.includes("Admin")
-        
-        User=Roles.includes("User")
-
-        if(Isadmin){
-                
-            save({Isadmin,Roles})
-
-        }
-        
-        else if(User){
-            save({User,Roles})
-
+        if (Isadmin) {
+            save({ Isadmin, Roles })
+        } else if (User) {
+            save({ User, Roles })
         }
        
-        return {Isadmin,User,id,username,Roles,account,update}
-
+        return { Isadmin, User, id, username, Roles, account, update }
     }
-    return {username:"",Roles:[],status,id:""}
-
+    return { Isadmin, User, id: "", username: "", Roles: [], account: "", update: "" } // Corrected return statement
 }
 
 export default UseAuth
